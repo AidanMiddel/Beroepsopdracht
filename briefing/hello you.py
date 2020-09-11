779b47d9efe6bbeaf31ce229663272119a8163fe
AidanMiddel <aidanm@ctjies.nl>
2020-09-11 16:22:12 +0200

    Hello you

--- briefing/hello you.py
+++ briefing/hello you.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 datum = datetime.now()
 import locale
+import os
+os.system('title Hello You')
 locale.setlocale(locale.LC_TIME, 'nl_NL')
 print("Hello You!, ik ben Aidan")
 print("De datum en tijd is: ", datum.strftime('%A %d %B %Y'))
@@ -8,5 +10,16 @@ print("Wie ben jij?")
 naamvanandere = input()
 if naamvanandere == "Aidan":
     print("hé jij ook hier")
+    print("Hoe grote kamer heb je?")
+    print("Hoe lang is jou kamer")
+    lengtekamer = int(input())
+    print("Hoe breed is jou kamer?")
+    breedtekamer = int(input())
+    grotekamer = breedtekamer * lengtekamer;
+    print("je kamer is: " , grotekamer , "m² groot")
 else:
     print("Hello " + naamvanandere)
+print(" ")
+
+os.system('pause')
+os.system('cls')
